04da36d1a076a136d8b1aec5761eea0e274f5c7e
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-23 13:52:09 +0200

    feat(fmt_8): Add (deprecated) fmt_8 to flake.nix to test building against it

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
+        extends.fmt_8 = final: _prev: { fmt = final.fmt_8; };
         extends.fmt_9 = final: _prev: { fmt = final.fmt_9; };
         extends.fmt_10 = final: _prev: { fmt = final.fmt_10; };
         extends.fmt_11 = final: _prev: { fmt = final.fmt_11; };
@@ -21,6 +22,19 @@
           (_final: _: {
             qt6.qtbase = null;
             qt6.wrapQtAppsHook = null;
+            fmt_8 = _final.fmt_9.overrideAttrs {
+              version = "8.1.1";
+              src = _final.fetchFromGitHub {
+                owner = "fmtlib";
+                repo = "fmt";
+                rev = "8.1.1";
+                hash = "sha256-leb2800CwdZMJRWF5b1Y9ocK0jXpOX/nwo95icDf308=";
+              };
+              cmakeFlags = [
+                (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+                (lib.cmakeBool "FMT_TEST" false)
+              ];
+            };
           })
         ];
 
